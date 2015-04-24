77eb61c9949c91d6f00b4954581a3576bdf4f1c4
SC <github.com@genkisugi.net>
2015-04-23 22:33:02 -0700

    start adding gulp; docs (but not on this branch)

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var jsdoc = require("gulp-jsdoc");
+
+gulp.task('default', function() {
+    console.log("'allo 'allo!");
+});
+
+gulp.task('doc', function() {
+    gulp.src("./bin/*.js")
+	.pipe(jsdoc('./doc'))
+});
