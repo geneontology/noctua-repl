3ab1486dd1e1634b90e8aed4bf7b7e96512d4d4d
SC <github.com@genkisugi.net>
2015-08-07 01:10:15 -0700

    ten at a time during testing

--- scripts/bulk-save-unsaved.repl.js
+++ scripts/bulk-save-unsaved.repl.js
@@ -5,10 +5,18 @@
 get_meta();
 
 var models_meta_ro = response.models_meta_read_only();
+
+var ten_count = 1;
 each(models_meta_ro, function(meta, mid){
+
     if( models_meta_ro[mid]['modified-p'] ){
 	console.log('Start saving: ' + mid);
 	manager.store_model(mid);
 	console.log('Done saving: ' + mid);
     }
+
+    if( ten_count === 10 ){
+	process.exit(0);
+    }
+    ten_count++;
 });
