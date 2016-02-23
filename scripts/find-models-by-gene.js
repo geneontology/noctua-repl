a9aa5acd87d909026fafaaf7f311c8116c2df694
SC <github.com@genkisugi.net>
2016-02-22 17:20:02 -0800

    simplifiy getting the model data now that we have access to owl_blob_json in the noctua_meta document; fixes geneontology/amigo#306

--- scripts/find-models-by-gene.js
+++ scripts/find-models-by-gene.js
@@ -111,40 +111,42 @@ meta_manager.register('search', function(resp, man){
 	/// IDs, we will get the model information available for them.
 	///
 
+	// Convert from model IDs to URIs (as found in the index).
+	var aunits_found = [];
+	each(models_found, function(mid){
+	    var muri = 'http://model.geneontology.org/' + mid.substr(-16);
+	    aunits_found.push(muri);
+	});
+
     	// For the sake of ease, we'll create a new manager.
 	var model_engine = new node_engine(golr_response);
 	var model_manager =
 		new golr_manager(endpoint_location, gconf, model_engine,'async');
-    	model_manager.set_personality('model_annotation');
+    	model_manager.set_personality('noctua_meta_model');
     	model_manager.set_facet_limit(0); // No longer want facets.
     	model_manager.set_results_count(max_model_count);
-    	model_manager.set_targets(models_found, ['model']);
+    	model_manager.set_targets(aunits_found, ['annotation_unit']);
 	
     	// On successful search, display a little model info.
     	model_manager.register('search', function(resp, man){
             
             // Look at all the models we got back.
-	    var already_seen_model = {};
             each(resp.documents(), function(doc){
 
-		if( doc && doc['model'] && doc['owl_blob_json'] ){
+		if( doc && doc['owl_blob_json'] ){
 		    
-		    // Check if we've seen it before.
-		    if( ! already_seen_model[doc['model']] ){
-
-			// Mark this model seen.
-			already_seen_model[doc['model']] = true;
-			
-			// Grab model JSON and convert it into an operable
-			// graph.
-			var jobj = JSON.parse(doc['owl_blob_json']);
-    			var graph = new noctua_graph();
-			graph.load_data_basic(jobj);
-						
-			// The final thing that we'll do.
-			//ll(graph.id());
-    			ll('Node count: ' + graph.all_nodes().length);
+		    // Grab model JSON and convert it into an operable
+		    // graph.
+		    var jobj = JSON.parse(doc['owl_blob_json']);
+    		    var graph = new noctua_graph();
+		    graph.load_data_basic(jobj);
+		    
+		    // The final thing that we'll do.
+    		    ll('Model: ' + doc['annotation_unit']);
+		    if( graph.id() ){
+    			ll(' Graph id: ' + graph.id());
 		    }
+    		    ll(' Node count: ' + graph.all_nodes().length);
 		}
     	    });
     	});
